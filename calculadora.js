1c68266827bb274c987f959d00ac0d08dcea7165
lorrany.canuto <lorrany.canuto@aluno.edu.pi.gov.br>
2026-06-18 17:12:42 +0000

    criei um produto

---
+++ calculadora.js
@@ -0,0 +1,17 @@
+function somarnumeros(n1,n2){
+    return (n1+n2);
+}
+
+function subtracao(a,b){
+    return a-b;
+}
+
+function multiplicar(a,b){
+    return a*b;
+}
+module.exports= multiplicar
+
+function dividir(a,b){
+    return a/b;
+module.exports= dividir
+}
---
+++ funcoes.js
@@ -0,0 +1,9 @@
+const calculadora={
+    numero 1:10,
+    numero 2:5,
+}
+    multiplicar(){
+        return  this.numero1 * this.numero2;
+    }
+
+console.log(calculadora.multiplicar());
